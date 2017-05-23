b93fe536b7a02f232ee55327f3995743d5617d40
Kresimir Condic <v-kresimir.condic@dump.hr>
2017-05-23 12:34:53 +0200

    Added first website and add student functionality.

---
+++ StudentBook/scripts/app/app.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('app', []);
+
+app.controller("StudentBookController",function($scope) {
+    $scope.students = [];
+    $scope.inputGender = "male";
+    $scope.studentLimit = false;
+
+    $scope.submit = function () {
+        $scope.students.push(
+            {
+                firstName: $scope.inputName,
+                lastName: $scope.inputLastName,
+                gender: $scope.inputGender,
+                entryDate: new Date(Math.random() * new Date())
+            });
+        $scope.inputName = "";
+        $scope.inputLastName = "";
+        if ($scope.students.length >= 10)
+            $scope.studentLimit = true;
+    }
+})
